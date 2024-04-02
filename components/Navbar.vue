9735911e89d8dbcba8f9105b43e12017492bf807
Kevin Schevalje <kevin.schevalje@mni.thm.de>
2024-04-03 01:58:58 +0200

    Created Landingpage components

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>Navbar</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <LandingpageHero />
+    <LandingpageFeatures />
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+  body{
+    margin: 0;
+    box-sizing: border-box;
+    border: 0;
+  }
+
+</style>
